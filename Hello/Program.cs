a3cf4add58ea471627cfaf32a06ee6439c3e4f21
MrZeng <857165956@qq.com>
2018-02-26 01:31:52 +0800

    修改

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -11,9 +11,13 @@ namespace Hello
         static void Main(string[] args)
         {
             // Write 输出不换行
+            Console.WriteLine("不换行");
             // WriteLine 输出换行
-            Console.WriteLine("HelloWorld");
+            Console.WriteLine("换行");
             // 等待、任意按键后继续执行
+            Console.ReadKey();
+            // ReadKey 输入会显示到屏幕上
+            Console.WriteLine("换行");
             Console.ReadKey();
         }
     }
